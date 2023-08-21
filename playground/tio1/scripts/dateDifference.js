7d3ab25140d1ddfe646b39a4a71a457e56910ff3
dgavriloff <denis.gavriloff@gmail.com>
2023-08-21 16:05:05 -0700

    added state to refresh tio count automatically

--- playground/tio1/scripts/dateDifference.js
+++ playground/tio1/scripts/dateDifference.js
@@ -13,13 +13,15 @@ export default function (currDate, endDate){
 	const finSecs = diffRS % 60
 	const finMin = diffRMin % 60
 	const finH = diffRH % 24
-	const finD = diffRD
+	const finD = diffRD % 365
 	const finW = diffRD / 7
 	const finMon = diffRY % 12
 	const finY = diffRY
 
 	const diffTypes = {
-		dhms : function() {return [[finD,finH,finMin,finSecs],["days", "hours", "minutes", "seconds"]]}
+		dhms : function() {return [[Math.floor(finD),Math.floor(finH),Math.floor(finMin),Math.floor(finSecs)],["days", "hours", "minutes", "seconds"]]},
+		ydhms : function() {return [[Math.floor(finY),Math.floor(finD),Math.floor(finH),Math.floor(finMin),Math.floor(finSecs)],["years","days", "hours", "minutes", "seconds"]]},
+		seconds : function(){return finSecs}
 	}
 
 
--- playground/tio1/src/App.jsx
+++ playground/tio1/src/App.jsx
@@ -1,37 +1,64 @@
 import  dateDifference  from '../scripts/dateDifference.js'
+import { useState, useEffect } from 'react'
+
 
 const App = () => {
-  
-  
   const endM = 0
   const endD = 5
-  const endY = 2024
+  const endY = 2025
   const endDate = new Date(endY, endM, endD, 0, 0, 0, 0)
-  const currDate = new Date()
+  const [ currDate, setCurrDate] = useState(new Date())
   
+  useEffect(() => {
+
+    const interval = setInterval(() => {
+      setCurrDate(new Date())
+    }, 999)
+
+    return () => clearInterval(interval)
+  }, [currDate])
   
   const dateDiff = dateDifference(currDate, endDate);
   
 
-
-
   return (
     <div>
       <Header />
-      <RawDateLine time = {dateDiff.dhms()[0][3]} type = {dateDiff.dhms()[1][3]}/>
+      <FullTime arr = {dateDiff.ydhms()}/>
+      <Button handleClick={() => setCurrDate(new Date())} text="Refresh"/>
     </div>
   )
 }
 
-const RawDateLine = (props) => {
+const FullTime = ({arr}) => {
   return(
-    <p>You have {Math.floor(props.time)} {props.type} left till you're out</p>
+    <div>
+    <TimeType time = {arr[0][0]} type = {arr[1][0]}/>
+    <TimeType time = {arr[0][1]} type = {arr[1][1]}/>
+    <TimeType time = {arr[0][2]} type = {arr[1][2]}/>
+    <TimeType time = {arr[0][3]} type = {arr[1][3]}/>
+    <TimeType time = {arr[0][4]} type = {arr[1][4]}/>
+    </div>    
   )
 }
 
 const Header = () => {
   return (
     <h1>Till I'm Out</h1>
+  )
+}
+
+const Button = ({handleClick, text}) => {
+  return (
+    <div>
+      <button onClick={handleClick}>{text}</button>
+    </div>
+  )
+}
+
+const TimeType = ({ time, type}) => {
+  return (
+    <p>{time} {type}</p>
   )
 }
 
